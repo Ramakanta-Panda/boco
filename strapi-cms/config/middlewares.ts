297ee4fe5c2fbe98cd05190cc2065f60899dab18
Ramakanta panda <ramakantapanda09@gmail.com>
2025-11-07 13:51:53 +0530

    Fix CORS and add brand content

--- strapi-cms/config/middlewares.ts
+++ strapi-cms/config/middlewares.ts
@@ -1,9 +1,21 @@
 export default [
-  'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'https://boco-strapi-cms.onrender.com',
+        'https://your-frontend-app.vercel.app'
+      ]
+    }
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
